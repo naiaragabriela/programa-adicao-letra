3c56df35a727ffdc4f1262b126d553f670d8d718
naiaragabriela <naigabi31@gmail.com>
2023-03-07 23:32:25 -0300

    exercicio 03

---
+++ src/ProgramaAdicaoLetra/ProgramaAdicaoLetra/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+Console.Write("Digite uma palavra: ");
+string palavra = Console.ReadLine();
+
+
+string nome = palavra.ToLower();
+string codigo = " ";
+
+for (int i = 0; i < nome.Length; i++)
+{
+    if (char.IsLetter(nome[i]))
+    {
+
+        switch (nome[i])
+        {
+            case 'a': 
+                codigo += nome[i];
+                
+                break;
+            case 'e':
+                codigo += nome[i];
+                break;
+            case 'i':
+                codigo += nome[i];
+                break;
+            case 'o':
+                codigo += nome[i];
+                break;
+            case 'u':
+                codigo += nome[i];
+                break;
+            default:
+                codigo += ("p"+ (nome[i]));
+                
+                break;
+        }
+
+    }
+
+}
+Console.WriteLine(codigo);
+
